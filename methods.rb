602e7817ad0cca11cdc49f91a445da9797b6923f
juanpablogil <jpablomgil@gmail.com>
2019-10-08 17:39:06 -0500

    update

--- methods.rb
+++ methods.rb
@@ -38,20 +38,20 @@ module Enumerable
   def my_count
     total = 0
     self.my_each do |i|
-      total += 1 if (yield(i))
+      total += 1 if yield(i)
     end
     total
   end
 end
 
 puts 'my_each'
-arrayx = [2,4,6,8,10]
+arrayx = [2, 4, 6, 8, 10]
 arrayx.my_each do |x|
   puts x * 2
 end
 puts 'my_each_with_index'
-arrayx = ["miden","uno","two","drei"]
-arrayx.my_each_with_index do |x,y|
+arrayx = %w[miden uno two drei]
+arrayx.my_each_with_index do |x, y|
   puts "#{x} = #{y}"
 end
 puts 'my_select'
@@ -60,17 +60,17 @@ arrayx.my_select do |x|
   puts x if x > 3
 end
 puts 'my_all'
-arr = ["Jasan", "John", "Jean", "Jelean"]
+arr = %w[jasa Joh Jean Jelean]
 arr.my_all? do |name|
-  name[-1] == "n"
+  name[-1] == 'n'
 end
 puts 'my_any'
-arr = ["jasa", "Joh", "Jean", "Jelean"]
+arr = %w[jasa Joh Jean Jelean]
 arr.my_any? do |name|
-  name[-1] == "n"
+  name[-1] == 'n'
 end
 puts 'my_none'
 arr = %w[jasa Joh Jean Jelean]
 arr.my_none? do |name|
-  name[-1] == "n"
+  name[-1] == 'n'
 end
