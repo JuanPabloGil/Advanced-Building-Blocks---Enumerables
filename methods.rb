c631b2b476de31cf2aa2f6c85f60cd83e5f4250d
juanpablogil <jpablomgil@gmail.com>
2019-10-10 12:19:45 -0500

    Bugs |my_inject|no block given yield|===> Fixed

--- methods.rb
+++ methods.rb
@@ -1,14 +1,14 @@
 module Enumerable
   def my_each
     for i in self
-      yield i
+      yield(i) if block_given?
     end
   end
 
   def my_each_with_index
     i = 0
     for j in self
-      yield j, i
+      yield(j, i) if block_given?
       i += 1
     end
   end
@@ -22,31 +22,43 @@ module Enumerable
   end
 
   def my_all?
-    self.my_each { |i| return true if yield(i) }
-    false
+    return true if !block_given?
+    if self.is_a? Hash
+      self.my_each { |m, n| return false if !yield(m, n) }
+    else
+      self.my_each { |i| return false if !yield(i) }
+    end
+    true
   end
 
   def my_any?
-    self.my_each { |i| return true if yield(i) }
+    return true if !block_given?
+    if self.is_a? Hash
+      self.my_each { |m, n| return true if yield(m, n) }
+    else
+      self.my_each { |i| return true if yield(i) }
+    end
     false
   end
 
   def my_none?
-    self.my_each { |i| return false if yield(i) }
+    return false unless block_given?
+    if self.class == Hash
+      self.my_each do |m, n|
+        return false if yield(m, n)
+      end
+    else
+      self.my_each do |i|
+        return false if yield(i)
+      end
+    end
     true
   end
 
-  def my_count(item = nil)
+  def my_count?
+    return self.length unless block_given?
     count = 0
-    for i in self
-      if !item.nil?
-        count += 1 if i == item
-      elsif block_given?
-        count += 1 if yield i
-      else
-        count += 1
-      end
-    end
+    self.my_each { |i| count += 1 if yield(i) }
     count
   end
 
@@ -59,62 +71,26 @@ module Enumerable
       end
     else
       for i in self
-        element = yield i
+        element = yield(i)
         arry << element
       end
     end
     arry
   end
 
-  def my_inject(initial = 0)
-    i = 0
-    accumulator = initial
-    while i < self.length
-      accumulator = yield(accumulator, self[i])
-      i += 1
-    end
-    accumulator
+  def my_inject(*start_num)
+      result = 0
+      if start_num.count == 0
+          self.my_each {|num|
+              result = yield(result, num)
+          }
+      return result
+      else
+          start_num = start_num[0]
+          self.my_each {|num|
+              start_num = yield(start_num, num)
+          }
+          return start_num
+      end
   end
 end
-
-puts 'my_each'
-arrayx = [2, 4, 6, 8, 10]
-arrayx.my_each do |x|
-  puts x * 2
-end
-puts 'my_each_with_index'
-arrayx = %w[miden uno two drei]
-arrayx.my_each_with_index do |x, y|
-  puts "#{x} = #{y}"
-end
-puts 'my_select'
-arrayx = [1, 2, 3, 4, 5]
-arrayx.my_select do |x|
-  puts x if x > 3
-end
-puts 'my_all'
-arrayx = %w[jasa Joh Jean Jelean]
-arrayx.my_all? do |name|
-  name[-1] == 'n'
-end
-puts 'my_any'
-arrayx = %w[jasa Joh Jean Jelean]
-arrayx.my_any? do |name|
-  name[-1] == 'n'
-end
-puts 'my_none'
-arrayx = %w[jasa Joh Jean Jelean]
-arrayx.my_none? do |name|
-  name[-1] == 'n'
-end
-puts 'my_count'
-arrayx = %w[jasa Joh Jean Jelean]
-puts arrayx.my_count
-puts 'my_map'
-arrayx = %w[jasa Joh Jean Jelean].my_map { |x| x + '!' }
-puts arrayx
-puts 'my_inject'
-def multiply_els(array)
-  array.my_inject(1) { |total, x| total * x }
-end
-puts multiply_els([2, 2, 4])
