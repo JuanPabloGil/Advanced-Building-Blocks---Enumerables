b055bbcaae65f7ee7fa06ad2a54a28066abeb00c
juanpablogil <jpablomgil@gmail.com>
2019-10-11 16:47:44 -0500

    Fixing Stickler Errors

--- methods.rb
+++ methods.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+#This module contains default methos of ruby: each, each_with_index, my_select, my_all?, my_any?, my_none?, my_count, my_map, my_injection
 module Enumerable
-
   def my_each
     return to_enum :my_each unless block_given?
     check_self = is_a?(Range) ? to_a : self
@@ -31,7 +32,7 @@ module Enumerable
     elsif param
       my_each { |x| unless param == x then all = false; break end }
     else
-      my_each { |x| unless x  then all = false; break end }
+      my_each { |x| unless x then all = false; break end }
     end
     all
   end
@@ -39,9 +40,9 @@ module Enumerable
   def my_any?(param = nil)
     any = false
     if block_given?
-      my_each { |x| if yield(x) then any=true; break end }
+      my_each { |x| if yield(x) then any = true; break end }
     elsif param
-      my_each { |x| if param == x then any=true; break end }
+      my_each { |x| if param == x then any = true; break end }
     else
       my_each { |x| if x then any=true; break end }
     end
@@ -49,12 +50,10 @@ module Enumerable
   end
 
   def my_none?(param = nil, &block)
-
     !my_any?(param, &block)
   end
 
   def my_count(param = nil)
-
     i = 0
     if block_given?
       my_each { |x| i += 1 if yield(x) == true }
@@ -75,11 +74,32 @@ module Enumerable
 
   def my_inject(param1 = nil, param2 = nil)
     check_self = is_a?(Range) ? to_a : self
-    accumulator = (param1.nil? || param1.is_a?(Symbol)) ? check_self[0] : param1
-    check_self[0..-1].my_each { |item| accumulator = yield(accumulator, item) } if block_given? && param1
-    check_self[1..-1].my_each { |item| accumulator = yield(accumulator, item) } if block_given? && !param1
-    check_self[1..-1].my_each { |i| accumulator = accumulator.send(param1, i) } if param1.is_a?(Symbol)
-    check_self[0..-1].my_each { |i| accumulator = accumulator.send(param2, i) } if param2
+    accumulator = param1.nil? || param1.is_a?(Symbol) ? check_self[0] : param1
+
+    if block_given? && param1
+      check_self[0..-1].my_each do |item|
+        accumulator = yield(accumulator, item)
+      end
+    end
+
+    if block_given? && !param1
+      check_self[1..-1].my_each do |item|
+        accumulator = yield(accumulator, item)
+      end
+    end
+
+    if param1.is_a?(Symbol)
+      check_self[1..-1].my_each do |i|
+        accumulator = accumulator.send(param1, i)
+      end
+    end
+
+    if param2
+      check_self[0..-1].my_each do |i|
+        accumulator = accumulator.send(param2, i)
+      end
+    end
     accumulator
+
   end
 end
